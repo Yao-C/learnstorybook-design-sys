27f4c95a782ed77d464c7feb8d761e547fe0b188
YAO CHENG <yao.cheng@norskluftambulanse.no>
2024-08-25 11:30:55 +0200

    chromatic and yml

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
         plugins: ['@emotion/babel-plugin'],
       },
     }),
+    visualizer(),
   ],
 });
